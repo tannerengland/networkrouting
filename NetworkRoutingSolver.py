59212806da973edb752c0cd5390938324753146f
Tanner England <tannerengland1@gmail.com>
2024-02-20 00:09:44 -0700

    comments added

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -3,7 +3,6 @@
 
 from CS312Graph import *
 import time
-import math
 import sys
 
 
@@ -20,41 +19,24 @@ class NetworkRoutingSolver:
 
     def getShortestPath( self, destIndex ):
         self.dest = destIndex
-        # TODO: RETURN THE SHORTEST PATH FOR destIndex
-        #       INSTEAD OF THE DUMMY SET OF EDGES BELOW
-        #       IT'S JUST AN EXAMPLE OF THE FORMAT YOU'LL 
-        #       NEED TO USE
         path_edges = []
         total_length = 0
-        # node = self.network.nodes[self.source]
-        # node = self.network.nodes[destIndex]
         currIndex = destIndex
-        # parentIndex = currIndex
-        # edges_left = len(self.prev)
         while currIndex != self.source:
-            # if parentIndex in self.prev and currIndex in self.prev:
             parentIndex = self.prev[currIndex]
-            # path_edges.append((self.id_to_node[parentIndex].loc, self.id_to_node[currIndex].loc, '{:.0f}'.format(math.sqrt((self.id_to_node[currIndex].loc.x()-self.id_to_node[parentIndex].loc.x())**2 + (self.id_to_node[currIndex].loc.y()-self.id_to_node[parentIndex].loc.y())**2))))
-            # math.sqrt((self.id_to_node[currIndex].loc.x()-self.id_to_node[parentIndex].loc.x())**2 + (self.id_to_node[currIndex].loc.y()-self.id_to_node[parentIndex].loc.y())**2)))
             currDist = self.dist[currIndex]
-            path_edges.append((self.id_to_node[parentIndex].loc, self.id_to_node[currIndex].loc, '{:.0f}'.format(currDist)))
+            try :
+                path_edges.append((self.id_to_node[parentIndex].loc, self.id_to_node[currIndex].loc, '{:.0f}'.format(currDist)))
+            except:
+                return {'cost':"unreachable", 'path':None}
             total_length += currDist
             currIndex = parentIndex
-            # total_length += ((self.id_to_node[currIndex].loc.x()-self.id_to_node[parentIndex].loc.x())**2 + (self.id_to_node[currIndex].loc.y()-self.id_to_node[parentIndex].loc.y())**2)
-
-            # total_length += self.id_to_node[parentIndex].length
-
-            # edge = node.neighbors[2]
-            # nextNode = self.prev[destIndex]
-            # path_edges.append((node.loc, nextNode.loc, '{:.0f}'.format(edge.length)))
-            # total_length += edge.length
-            # node = edge.dest
-            # edges_left -= 1
-        # return {'cost':self.dist[destIndex], 'path':path_edges}
         return {'cost':total_length, 'path':path_edges}
 
-
+    # Time: O(𝐷elete𝑀in⋅|𝑉|+𝐷ecrease𝐾ey⋅|𝐸|)
+    # Space: O(|𝑉|)
     def computeShortestPaths( self, srcIndex, use_heap=False ):
+        # O(1) Initializing variables
         self.source = srcIndex
         t1 = time.time()
         pq = None
@@ -62,88 +44,81 @@ class NetworkRoutingSolver:
             pq = PriorityQueueBinaryHeap()
         else:
             pq = PriorityQueueArray()
+        # O(𝑀ake𝑄ueue) varies based on implementation (nlogn for heap, n for array)
         pq.make_queue(self.network.nodes)
         srcNode = self.network.nodes[srcIndex].node_id
+        # Time: O(𝐷elete𝑀in⋅|𝑉|+𝐷ecrease𝐾ey⋅|𝐸|)
+        # Space: O(|𝑉|)
         self.dist, self.prev = self.Dijkstra(pq, srcNode)
-        # TODO: RUN DIJKSTRA'S TO DETERMINE SHORTEST PATHS.
-        #       ALSO, STORE THE RESULTS FOR THE SUBSEQUENT
-        #       CALL TO getShortestPath(dest_index)
-
         t2 = time.time()
         return (t2-t1)
 
+# Finds shortest path from a source node
+# Time: O(𝐷elete𝑀in⋅|𝑉|+𝐷ecrease𝐾ey⋅|𝐸|) runs delete minimum for every node, and delete key for every edge
+# Space: O(|𝑉|) stores all nodes
     def Dijkstra(self, pq, srcNode):
+        # O(1) Intializing variables
         prev = {}
         dist = {}
+        # O(|𝑉|) Assign all nodes distances to infinity and prev to nothing
         for curr in self.network.nodes:
             dist[curr.node_id] = sys.maxsize
             prev[curr.node_id] = None
+        # O(𝐷ecrease𝐾ey) varies based on implementation (logn for heap, n for array)
         pq.decrease_dist(srcNode,0)
         dist[srcNode] = 0
         while not pq.is_empty():
+            # O(Delete𝑀in⋅|𝑉|) removes minimum for each node based on implementation (logn for heap, n for array)
             currIndex, currDist = pq.delete_min()
             currNode = self.id_to_node[currIndex]
+            # O(𝐷ecrease𝐾ey * |E|) runs decrease distance on each edge to current node
             for destNode in currNode.neighbors:
                 if destNode.dest.node_id in pq.distances:
-                    # if pq.distances.get(destNode.dest.node_id) > (currDist + math.sqrt((currNode.loc.x()-destNode.dest.loc.x())**2 + (currNode.loc.y()-destNode.dest.loc.y())**2)):
                     if pq.distances.get(destNode.dest.node_id) > (currDist + destNode.length):
-
                         prev[destNode.dest.node_id] = currIndex
-                        # pq.decrease_dist(destNode.dest.node_id, (currDist + math.sqrt((currNode.loc.x()-destNode.dest.loc.x())**2 + (currNode.loc.y()-destNode.dest.loc.y())**2)))
                         pq.decrease_dist(destNode.dest.node_id, (currDist + destNode.length))
-                        # pq.decrease_dist(destNode.dest.node_id, (destNode.length))
-
-                        # dist[destNode.dest.node_id] = (currDist + math.sqrt((currNode.loc.x()-destNode.dest.loc.x())**2 + (currNode.loc.y()-destNode.dest.loc.y())**2))
                         dist[destNode.dest.node_id] = pq.distances[destNode.dest.node_id] - currDist
+        # Space: O(|𝑉|) to store all nodes in graph with distances and previous nodes
         return dist, prev
 
-
+# Priority Queue using an array implementation
+# Time: O(|V|) runs for every node in worst case
+# Space: O(|V|) holds every node and associated distance
 class PriorityQueueArray:
     def __init__(self):
+        # stores node and given distance
         self.distances = {}
 
+    # O(1) simple check to assure PQ is empty
     def is_empty(self):
         if len(self.distances) == 0:
             return True
         else:
             return False
-        # return bool(self.distances)
 
+    # O(1) inserts node at the end of array
     def insert(self, node, dist):
         self.distances[node] = dist
-        # self.queue[node,dist]
 
+    # O(1) modifies one of the distances to a node
     def decrease_dist(self, node, dist):
-        if node in self.distances:
-            if dist < self.distances[node]:
-                self.distances[node] = dist
-        # else:
-        #     print("Key not found in the priority queue.")
+        self.distances[node] = dist
 
+    # O(|V|) searches through all nodes (worst case) until finds minimum and deletes it
     def delete_min(self):
-        # if not self.distances:
-        #     print("Priority queue is empty.")
-        #     return None
-
         min_node = min(self.distances, key=self.distances.get)
         currDist = self.distances[min_node]
         self.distances.pop(min_node)
         return min_node, currDist
 
-    # def make_queue(self, solver: NetworkRoutingSolver):
-    #     for node in solver.network:
-    #         self.queue[node] = (float('inf'))
-    # def make_queue(self, nodes):
-    #     for node in nodes:
-    #         self.queue[node] = (float('inf'))
+    # O(|V|) stores in PQ for every node in graph, stores infinity as its distances
     def make_queue(self, nodes):
-        # self.queue = {key: float('inf') for key, _ in nodes}
-        # self.queue = dict(nodes)
         for node in nodes:
-            # self.insert(node.node_id, float('inf'))
             self.insert(node.node_id, sys.maxsize)
 
-
+# Priority Queue using a binary heap implementation
+# Time: O(|V|log|V|) runs for every node in worst case
+# Space: O(|V|) holds every node and associated distance and index
 class PriorityQueueBinaryHeap:
     def __init__(self):
         # store nodes in order
@@ -153,22 +128,14 @@ class PriorityQueueBinaryHeap:
         # stores indices of pq with node
         self.indices = {}
 
+    # O(1) simple check to assure PQ is empty
     def is_empty(self):
         if len(self.distances) == 0:
             return True
         else:
             return False
-        # return bool(self.distances)
 
-            # def swap(self, parent, child):
-    #     parentIndex = self.indices.get(parent)
-    #     childIndex = self.indices.get(child)
-    #     self.indices[parent] = childIndex
-    #     self.indices[child] = parentIndex
-    #     tempParentNode = self.queue[parentIndex]
-    #     self.queue[parentIndex] = self.queue[childIndex]
-    #     self.queue[childIndex] = tempParentNode
-
+    # O(1) simple swap within all given private data members of nodes
     def swap(self, node1, node2):
         node1Index = self.indices.get(node1)
         node2Index = self.indices.get(node2)
@@ -178,15 +145,19 @@ class PriorityQueueBinaryHeap:
         self.queue[node1Index] = self.queue[node2Index]
         self.queue[node2Index] = tempNode
 
+    # O(1) calculates index for left child
     def getLChildIndex(self, index):
         return ((index + 1) * 2) - 1
 
+    # O(1) calculates index for right child
     def getRChildIndex(self, index):
         return ((index + 1) * 2) + 1 - 1
 
+    # O(1) calculates index for parent
     def getParentIndex(self, index):
         return ((index+1)//2)-1
 
+    # O(log|V|) finds proper place based on distance in tree by bubbling up from lower in the tree
     def bubbleUp(self, curr_index):
         while curr_index > 0:
             parent = self.queue[self.getParentIndex(curr_index)]
@@ -197,6 +168,7 @@ class PriorityQueueBinaryHeap:
             else:
                 break
 
+    # O(log|V|) finds proper place based on distance in tree by bubbling down from higher in the tree
     def bubbleDown(self):
         curr_index = 0
         curr_node = self.queue[curr_index]
@@ -207,11 +179,9 @@ class PriorityQueueBinaryHeap:
         while True:
             if R is not None:
                 if (self.distances[L] < self.distances[curr_node]) and (self.distances[L] < self.distances[R]):
-                    # if self.distances[L] < self.distances[R]:
                         self.swap(L, curr_node)
                         curr_index = self.getLChildIndex(curr_index)
                 elif (self.distances[R] < self.distances[curr_node]) and (self.distances[L] > self.distances[R]):
-                    # if self.distances[L] > self.distances[R]:
                         self.swap(R, curr_node)
                         curr_index = self.getRChildIndex(curr_index)
                 elif (self.distances[curr_node] <= self.distances[L]) and (self.distances[curr_node] <= self.distances[R]):
@@ -234,7 +204,7 @@ class PriorityQueueBinaryHeap:
             else:
                 R = None
 
-
+    # O(log|V|) inserts a node into tree and utilizes bubbleUp function to find proper place based on distance
     def insert(self, node, dist):
         self.queue.append(node)
         self.distances[node] = dist
@@ -242,20 +212,19 @@ class PriorityQueueBinaryHeap:
         curr_index = len(self.queue) - 1
         self.bubbleUp(curr_index)
 
+    # O(log|V|) decreases key of a node within the tree and utilizes bubbleUp function to find proper place based on distance
     def decrease_dist(self, node, dist):
-        # if node in self.queue:
         if dist < self.distances[node]:
             self.distances[node] = dist
-        # curr_index = len(self.queue) - 1
         curr_index = self.indices[node]
         self.bubbleUp(curr_index)
 
+    # O(log|V|) deletes minimum node in a tree and utilizes bubbleDown function to find proper place based on distance
     def delete_min(self):
         self.swap(self.queue[0], self.queue[len(self.queue) - 1])
         currMin = self.queue[len(self.queue) - 1]
         currDist = self.distances[currMin]
         min_node = self.queue.pop(len(self.queue) - 1)
-        # currDist = self.distances[min_node]
         del self.distances[min_node]
         del self.indices[min_node]
         if len(self.queue) > 0:
@@ -263,59 +232,7 @@ class PriorityQueueBinaryHeap:
 
         return min_node, currDist
 
+    # O(|V|log|V|) creates PQ by inserting every node(|V|) and intializing its distance to infinity, insert utilizes bubbleUp function (O(log|V|))
     def make_queue(self, nodes):
-        # self.queue = {key: float('inf') for key, _ in nodes}
         for node in nodes:
-            # self.insert(node.node_id, float('inf'))
             self.insert(node.node_id, sys.maxsize)
-            # self.insert(node, sys.maxsize)
-
-
-# pq = PriorityQueueArray()
-# pq.insert('A', 2)
-# pq.insert('B', 4)
-# pq.insert('C', 2)
-#
-# print("Initial Priority Queue:", pq.queue)
-#
-# pq.decrease_dist("A", 3)
-# print("After decreasing value of A:", pq.queue)
-#
-# min_node = pq.delete_min()
-# print(f"Deleted minimum value: ({min_node})")
-# print("Updated Priority Queue:", pq.queue)
-#
-# elements = [("D", 1), ("E", 8), ("F", 4)]
-# pq.make_queue(elements)
-# print("Priority Queue after make_queue:", pq.queue)
-
-# pq = PriorityQueueBinaryHeap()
-#
-# # pq.make_queue(["a", "b", "c", "d"])
-# pq.insert('9',9)
-# pq.insert('8',8)
-# pq.insert('3',3)
-# pq.insert('5',5)
-# pq.insert('6',6)
-# pq.insert('4',4)
-# pq.insert('2',2)
-# pq.insert('1',1)
-# pq.insert('10',10)
-# pq.insert('14',14)
-# pq.insert('7',7)
-# pq.insert('12',12)
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min() #youch
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min()
-# pq.delete_min()
-
-
-
-# pq.decrease_dist('b', .5)
-
